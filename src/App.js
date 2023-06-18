79a35c0d4d071c622756f703e4037e6dc2328ed5
Mj-Njuguna <jessemike0002@gmail.com>
2023-06-18 22:58:12 +0300

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
-import Home from './Home'
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    //BEM naming convention
-    
     <div className="app">
-
-        
-        <Header/>
-        <Home/>
-        
-      
+      <Router>
+        <Routes>
+            <Route path='/' element={<>
+            <Header /> <Home /></>} //<></> react fragment works like a div
+            />
+            <Route path="/checkout" element={<><Header /><h1>I AM A CHECKOUT 💯</h1></>
+            } 
+            />
+            <Route path='*'
+             element = {<><Header /> <Home /></>}
+            />
+        </Routes>
+      </Router>
     </div>
-    
   );
 }
 
 export default App;
-
