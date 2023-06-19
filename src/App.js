51c3cfcc4e022d9c1c465b89d636a48e93afccda
Mj_Njuguna <jessemike0002@gmail.com>
2023-06-19 13:13:41 +0300

    UPDATE

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import {BrowserRouter as Router,Route, link, Routes} from 'react-router-dom'
 function App() {
   return (
     //BEM naming convention
     
     <div className="app">
-
-        
-        <Header/>
-        <Home/>
+  <Routes>
+    <Route></Route>
+          <Header/>
+          <Home/>
+    
+  </Routes>
         
       
     </div>
